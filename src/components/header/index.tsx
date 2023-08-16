80cb00e423ff82736f7f1a5a3116ae78b613fa85
gabrilgvbf <gabrilgvbf@gmail.com>
2023-08-15 22:06:52 -0300

    Initial

---
+++ src/components/header/index.tsx
@@ -0,0 +1,9 @@
+import { SHeader } from "./styles"
+const Header = () => (
+    <SHeader>
+
+    </SHeader>
+)
+
+
+export default Header
---
+++ src/components/header/styles.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+export const SHeader = styled.header`
+
+background: linear-gradient(0deg, rgba(230, 103, 103, 0.05), rgba(230, 103, 103, 0.05)),
+linear-gradient(0deg, #FFEBD9, #FFEBD9);
+
+`
